e4b12010567b9c8cf4517e50d3099657b64f4c7b
ongtrumphuong <ongtrumactive.is.hackerpdp@gmail.com>
2024-08-17 21:00:56 +0700

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Header from './Components/Header'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Slider from './Components/Slider'
 import './App.css'
 
 function App() {
@@ -11,6 +10,7 @@ function App() {
     <>
       <div>
         <Header/>
+        <Slider/>
       </div>
     </>
   )
