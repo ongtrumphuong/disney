59ef296fe095fdfecef0bcb6474bd9efc3e044b2
ongtrumphuong <ongtrumactive.is.hackerpdp@gmail.com>
2024-08-17 19:03:36 +0700

    responsive navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../Images/logo.png'
 import user from './../Images/man.png'
 import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv } from 'react-icons/hi2'
@@ -6,6 +6,7 @@ import { HiPlus, HiDotsVertical } from 'react-icons/hi'
 import HeaderItem from './HeaderItem'
 
 function Header() {
+  const [toggle, setToggle] = useState(false)
   const menu = [
     {
       name: 'HOME',
@@ -36,9 +37,25 @@ function Header() {
     <div className='flex items-center justify-between p-5'>
       <div className='flex gap-8 items-center'>
         <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
-        {menu.map((item) => (
-          <HeaderItem name={item.name} Icon={item.icon} />
-        ))}
+        <div className='hidden md:flex gap-8'>
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className='flex md:hidden gap-5'>
+          {menu.map((item, index) => index < 3 && (
+            <HeaderItem name={''} Icon={item.icon} />
+          ))}
+        </div>
+        <div className='md:hidden' onClick={() => setToggle(!toggle)}>
+          <HeaderItem name={''} Icon={HiDotsVertical} />
+          {toggle ? 
+            <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+              {menu.map((item, index) => index > 2 && (
+                <HeaderItem name={item.name} Icon={item.icon} />
+              ))}
+            </div> : null }
+        </div>
       </div>
       <img src={user} className='w-[40px] rounded-full'/>
     </div>
