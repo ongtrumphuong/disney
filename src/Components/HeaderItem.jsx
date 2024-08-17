7d8c6553e4e07cd769154784204e7716dab6841e
ongtrumphuong <ongtrumactive.is.hackerpdp@gmail.com>
2024-08-17 15:10:06 +0700

    update navbar

---
+++ src/Components/HeaderItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeaderItem({ name, Icon }) {
+  return (
+    <div className='flex items-center text-white gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8'>
+        <Icon/>
+        <h2>{name}</h2>
+    </div>
+  )
+}
+
+export default HeaderItem
