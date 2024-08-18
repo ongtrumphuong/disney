2863c07b704f35691a1b64eb71f6e59aa7f537f0
ongtrumphuong <ongtrumactive.is.hackerpdp@gmail.com>
2024-08-18 20:26:37 +0700

    update genres

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../Services/GlobalApi'
 import MovieCard from './../Components/MovieCard'
 import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
+import HrMovieCard from './HrMovieCard';
 
-function MovieList({genreId}) {
+function MovieList({genreId, index_}) {
     const [movieList, setMovieList] = useState([])
 
     const elementRef = useRef(null)
@@ -29,16 +30,21 @@ function MovieList({genreId}) {
   return (
     <div className='relative'>
        <IoChevronBackOutline onClick={()=>slideLeft(elementRef.current)} 
-         className={`text-[50px] text-white  p-2 z-10 cursor-pointer hidden md:block absolute mt-[150px]`}/>
+         className={`text-[50px] text-white p-2 z-10 cursor-pointer hidden md:block absolute
+            ${index_%3==0?'mt-[80px]':'mt-[150px]'} `}/>
             
       <div ref={elementRef} className='flex overflow-x-auto gap-8 scrollbar-none scroll-smooth pt-5 px-3 pb-5'>
         {movieList.map((item, index) => (
-          <MovieCard movie={item}/>
+          <>
+           {index_%3==0?<HrMovieCard movie={item}/>:<MovieCard movie={item} />}
+          </>
+         
         ))}
       </div>
 
       <IoChevronForwardOutline onClick={()=>slideRight(elementRef.current)}
-           className={`text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 top-0 absolute right-0 mt-[150px] `}/> 
+           className={`text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 top-0 absolute right-0 
+            ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}/> 
     </div>
     
   )
